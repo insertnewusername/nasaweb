f08e81702a0aa075658e42f9c611fe27ee39d92f
IMMAHUMAN <yuchenliu.matthew@gmail.com>
2026-07-11 16:57:41 +1000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 const API_KEY = import.meta.env.VITE_NASA_API_KEY;
+document.querySelector("#app").innerHTML = "<p>loading...</p>";
+
+
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+})
