55923565846c8632717ef68e7ed81a1d2e339c79
IMMAHUMAN <yuchenliu.matthew@gmail.com>
2026-07-13 11:50:43 +1000

    make it load image and header

--- src/main.js
+++ src/main.js
@@ -7,3 +7,15 @@ fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
 .then(data => {
     console.log(data);
 })
+
+.then(data => {
+    document.querySelector("#app").innerHTML = `${data.title}`;
+})
+
+.then(data => {
+    document.querySelector("#app").innerHTML = `
+        <h1>${data.title}</h1>
+        <img src="${data.url}" />
+        <p>${data.explanation}</p>
+    `;
+})
