7e74810e31d039115e6ae0752e85a7048bd39a67
IMMAHUMAN <yuchenliu.matthew@gmail.com>
2026-07-13 13:43:06 +1000

    Make it show vid or img

--- src/main.js
+++ src/main.js
@@ -6,10 +6,24 @@ fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
 .then(response => response.json())
 .then(data => {
     console.log(data);
-    document.querySelector("#app").innerHTML = `
+    
+    
+    let media;
+
+    if (data.media_type === "image") {
+     media = `<img src="${data.url}"/>`;
+     } else {
+    media = `<video src="${data.url}" controls></video>`;
+     }
+     
+    
+     document.querySelector("#app").innerHTML = `
     <h1>${data.title}</h1>
-    <img src="${data.url}" />
+    ${media}
     <p>${data.explanation}</p>
     `;
 })
 
+.catch(err => {
+    document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`;
+});
