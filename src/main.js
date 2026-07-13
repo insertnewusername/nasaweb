1aaf0cd9b967e37144c8b8da2b1c0d310441835c
IMMAHUMAN <yuchenliu.matthew@gmail.com>
2026-07-13 11:52:19 +1000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,12 +6,10 @@ fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
 .then(response => response.json())
 .then(data => {
     console.log(data);
-})
-
-.then(data => {
     document.querySelector("#app").innerHTML = `
-        <h1>${data.title}</h1>
-        <img src="${data.url}" />
-        <p>${data.explanation}</p>
+    <h1>${data.title}</h1>
+    <img src="${data.url}" />
+    <p>${data.explanation}</p>
     `;
 })
+
